093ce1bbbb5e06197c849b3d5af7e30cd98dd13c
Urswin-Faro <urswinf@gmail.com>
2025-02-11 11:34:09 +0200

    Add files via upload

--- frontendproject/src/store/index.js
+++ frontendproject/src/store/index.js
@@ -2,21 +2,91 @@ import { createStore } from 'vuex'
 /* eslint-disable */
 export default createStore({
   state: {
-    employees:null
+    employees:null,
+    attendance: null,
   },
   getters: {
   },
+  // commit
   mutations: {
     setEmployees(state,payload){
-      state.employees=payload
+      state.employees = payload
+    },
+    setAttendance(state, payload) {
+      state.attendance = payload
     }
   },
+
+  // dispatch
   actions: {
-    async getEmploy({ commit }) {
-      const {message} = await (await fetch('http://localhost:3030/employ')).json()
-      commit('setEmployees',message)
+    async getEmployees({commit},payload){
+      let {employees} = await (await fetch("http://localhost:3030/Employees")).json()
+      // let info = await employees.json()
+
+      console.log(employees)
+      commit('setEmployees',employees)
+    },
+    async getAttendance({ commit }) {
+      const { attendance } = await (await fetch('http://localhost:3030/attendance')).json()
+      commit('setAttendance', attendance)
+    },
+    async postAttendance({ commit }, attendant) {
+      console.log(attendant);
+      await fetch('http://localhost:3030/attendance', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          employee_id: attendant.employee_id,
+          date: attendant.date,
+          status: attendant.status
+        })
+      })
+      await this.dispatch('getAttendance');
+    },
+    async deleteEmployee({commit},employee_id){
+    await fetch("http://localhost:3030/Employees/"+ employee_id,{method:"DELETE"})
+    console.log(employee_id);
+    },
+    
+async postEmployee({commit},employee){
+  console.log(employee);
+  await fetch("http://localhost:3030/Employees/",{
+  method:"POST",
+  headers:{ 
+    "Content-type":"application/json"
   },
+  body: JSON.stringify({
+    employee_id:employee.employee_id,
+    name: employee.name,
+    department: employee.department,
+    position: employee.position,
+    salary: employee.salary,
+    employment_history: employee.employment_history,
+    email: employee.email
+  }),
+  })
 },
-  modules: {
-  }
+async updateEmployee({commit},employee){
+  console.log(employee);
+  await fetch("http://localhost:3030/Employees/"+ employee.employee_id,{
+  method:"PUT",
+  headers:{ 
+    "Content-type":"application/json"
+  },
+  body: JSON.stringify({
+    employee_id:employee.employee_id,
+    name: employee.name,
+    position: employee.position,
+    department: employee.department,
+    salary: employee.salary,
+    employment_history: employee.employment_history,
+    email: employee.email
+  })
+  })
+}
+},
+ modules: {
+  },
 })
